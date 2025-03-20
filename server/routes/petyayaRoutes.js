bbe19d7ed2e125218054f9d6eb287f7b3f64163e
jsc1989 <jsc1989@proton.me>
2025-03-20 23:22:47 +0800

    Removed conflicting updates manually after merging

--- server/routes/petyayaRoutes.js
+++ server/routes/petyayaRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const petyayaController = require('../controllers/petyayaController')
 const { ensureAuthenticated } = require('../middleware/auth');
+const Post = require('../../models/posts');
 
 router.get('/', petyayaController.index)
 // Routes now protected (need to login to access)
@@ -9,62 +10,19 @@ router.get('/feed', ensureAuthenticated, petyayaController.posts);
 router.post('/add-post', ensureAuthenticated, petyayaController.addPost);
 router.put('/edit-post/:id', ensureAuthenticated, petyayaController.editPost);
 router.delete('/delete-post/:id', ensureAuthenticated, petyayaController.deletePost);
-const petyayaController = require('../controllers/petyayaController');
-const Post = require('../../models/posts');
 
-// Home route (index page)
-router.get('/', petyayaController.index);
-
-// ---------------------- Post Routes ----------------------
-
-// Feed page that displays all posts
-router.get('/feed', petyayaController.posts);
-
-// Add a new post
-router.post('/add-post', petyayaController.addPost);
-
-// Edit a post
-router.post('/edit-post/:id', async (req, res) => {
-    await Post.findByIdAndUpdate(req.params.id, { content: req.body.content });
-    res.redirect('/feed');
-});
-
-// Delete a post
-router.delete('/delete-post/:id', petyayaController.deletePost);
 
 // Like a post
 router.post('/like-post/:id', petyayaController.likePost);
-
-// ---------------------- Comment Routes ----------------------
 
 // Add a comment to a post
 router.post('/add-comment/:id', petyayaController.addComment);
 
 // Edit a comment
-router.post('/edit-comment/:id', async (req, res) => {
-    await Post.updateOne(
-      { "comments._id": req.params.id },
-      { $set: { "comments.$.text": req.body.content } }
-    );
-    res.redirect('/feed');
-});
+router.post('/edit-comment/:id', petyayaController.editComment);
 
 // Delete a comment
-router.delete("/delete-comment/:commentId", async (req, res) => {
-    try {
-        const { commentId } = req.params;
-        const post = await Post.findOne({ "comments._id": commentId });
-
-        if (!post) return res.status(404).send("Comment not found");
-
-        post.comments = post.comments.filter(comment => comment._id.toString() !== commentId);
-        await post.save();
-        res.redirect("/feed");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Server Error");
-    }
-});
+router.delete("/delete-comment/:commentId", petyayaController.deleteComment);
 
 // Like a comment
 router.post('/like-comment/:commentId', petyayaController.likeComment);
@@ -78,35 +36,10 @@ router.post("/reply-to-comment/:commentId", petyayaController.addReplyToComment)
 router.post('/like-reply/:replyId', petyayaController.likeReply);
 
 // Edit a reply
-router.post('/edit-reply/:id', async (req, res) => {
-    await Post.updateOne(
-      { "comments.replies._id": req.params.id },
-      { $set: { "comments.$[].replies.$[reply].text": req.body.content } },
-      { arrayFilters: [{ "reply._id": req.params.id }] }
-    );
-    res.redirect('/feed');
-});
+router.post('/edit-reply/:id', petyayaController.editReply);
 
 // Delete a reply
-router.delete("/delete-reply/:commentId/:replyId", async (req, res) => {
-    try {
-        const { commentId, replyId } = req.params;
-        const post = await Post.findOne({ "comments._id": commentId });
-
-        if (!post) return res.status(404).send("Comment not found");
-
-        const comment = post.comments.find(comment => comment._id.toString() === commentId);
-        if (comment) {
-            comment.replies = comment.replies.filter(reply => reply._id.toString() !== replyId);
-        }
-
-        await post.save();
-        res.redirect("/feed");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Server Error");
-    }
-});
+router.delete("/delete-reply/:commentId/:replyId", petyayaController.deleteReply);
 
 // Reply to a reply
 router.post('/reply-to-reply/:postId/:commentId/:replyId', petyayaController.addReplyToReply);
