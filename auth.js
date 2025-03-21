ee0e5a5845e38c62115a506859b0dc15aae0aadf
jsc1989 <jsc1989@proton.me>
2025-03-21 13:46:03 +0800

    Add Render callback url

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ const User = require('./models/user'); // Ensure this path is correct
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback',
+  callbackURL: 'https://petyaya.onrender.com/auth/google/callback' || 'http://localhost:3000/auth/google/callback',
   passReqToCallback: true // Allows us to access request in the callback
 }, async (request, accessToken, refreshToken, profile, done) => {
   try {
